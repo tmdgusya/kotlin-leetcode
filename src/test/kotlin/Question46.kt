28200b66310464d528dfd90a50bfaaf9dbc19bfd
tmdgusya <tmdgusya@naver.com>
2022-10-06 22:11:18 +0900

    Solution 46
    https://leetcode.com/problems/permutations/

--- src/test/kotlin/Question46.kt
+++ src/test/kotlin/Question46.kt
@@ -19,6 +19,29 @@ class Question46 : FunSpec({
         )
     }
 
+    test("case 02") {
+        val given = intArrayOf(1,2,3,4)
+
+        val result = permute(given)
+
+        result shouldBe listOf(
+            listOf(1,2,3,4),
+            listOf(1,2,4,3),
+            listOf(1,3,2,4),
+            listOf(1,3,4,2),
+            listOf(1,4,2,3),
+            listOf(1,4,3,2),
+            listOf(2,1,3,4),
+            listOf(2,1,4,4),
+            listOf(2,3,1,4),
+            listOf(2,3,4,1),
+            listOf(2,4,1,3),
+            listOf(2,4,3,1),
+            listOf(3,2,1),
+            listOf(3,1,2)
+        )
+    }
+
 })
 
 fun permute(nums: IntArray): List<List<Int>> {
