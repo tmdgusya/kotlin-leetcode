ec3c3f9c896ecde2b427f9be7a6208552f4815b9
tmdgusya <tmdgusya@naver.com>
2023-03-26 20:16:15 +0900

    Leetcode 46

--- src/test/kotlin/Question46.kt
+++ src/test/kotlin/Question46.kt
@@ -23,8 +23,7 @@ class Question46 : FunSpec({
         val given = intArrayOf(1,2,3,4)
 
         val result = permute(given)
-
-        result shouldBe listOf(
+        val expected = listOf(
             listOf(1,2,3,4),
             listOf(1,2,4,3),
             listOf(1,3,2,4),
@@ -40,29 +39,54 @@ class Question46 : FunSpec({
             listOf(3,2,1),
             listOf(3,1,2)
         )
+        println(result)
+        println(expected.size)
+
+        result shouldBe expected
     }
 
 })
 
+//fun permute(nums: IntArray): List<List<Int>> {
+//    val result = mutableListOf<MutableList<Int>>()
+//    val tempList = mutableListOf<Int>()
+//    for (num in nums) tempList.add(num)
+//    backTracking(nums.size, result, tempList, 0)
+//    return result
+//}
+//
+//fun backTracking(
+//    size: Int,
+//    result: MutableList<MutableList<Int>>,
+//    tempList: MutableList<Int>,
+//    start: Int) {
+//    if (start == size) result.add(ArrayList(tempList))
+//
+//    for (i in start until size) {
+//        // start Index 값이 제일 앞으로 오도록 변경
+//        Collections.swap(tempList, start, i)
+//        backTracking(size, result, tempList, start + 1)
+//        Collections.swap(tempList, start, i)
+//    }
+//}
+
 fun permute(nums: IntArray): List<List<Int>> {
-    val result = mutableListOf<MutableList<Int>>()
-    val tempList = mutableListOf<Int>()
-    for (num in nums) tempList.add(num)
-    backTracking(nums.size, result, tempList, 0)
-    return result
+    val resultSet: MutableSet<MutableList<Int>> = mutableSetOf()
+    val max = nums.size
+    comb(nums, mutableListOf(), resultSet, 0, max)
+    return resultSet.toList()
 }
 
-fun backTracking(
-    size: Int,
-    result: MutableList<MutableList<Int>>,
-    tempList: MutableList<Int>,
-    start: Int) {
-    if (start == size) result.add(ArrayList(tempList))
+fun comb(array: IntArray, acc: MutableList<Int>, resultSet: MutableSet<MutableList<Int>>, depth: Int, max: Int) {
+    if (depth == max) {
+        resultSet.add(acc)
+        return
+    }
 
-    for (i in start until size) {
-        // start Index 값이 제일 앞으로 오도록 변경
-        Collections.swap(tempList, start, i)
-        backTracking(size, result, tempList, start + 1)
-        Collections.swap(tempList, start, i)
+    for (ele in array) {
+        if (ele in acc) continue
+        val copied = acc.toMutableList()
+        copied.add(ele)
+        comb(array, copied, resultSet, depth + 1, max)
     }
 }
